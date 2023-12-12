1fc45b32d8f2937cd400b33c419cd0220b48cb67
Mary Sigmon <marysigmon@Marys-MacBook-Pro-2.local>
2023-12-11 20:23:16 -0500

    added final element but still getting error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+  console.log(puppyList)
+
   return (
     <>
-      <div>
+      <div className="App">
+
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={handleClick} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })}
+
+        {featPupId && (
+          <div className='PupDetails'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
         
       </div>
       
